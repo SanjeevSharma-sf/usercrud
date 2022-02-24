d9d90d36b66a7f488132db5129819501a9800037
Sanjeev Sharma <sanjeevsharma1603@gmail.com>
2022-02-24 13:52:05 +0530

    server made up

---
+++ server.js
@@ -0,0 +1,30 @@
+const app = require("./app");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const bodyparser = require("body-parser");
+const path = require("path");
+//config
+dotenv.config({ path: "server/config/config.env" });
+
+//log requests
+app.use(morgan("tiny"));
+
+//parse request to body-parser
+app.use(bodyparser.urlencoded({ extended: true }));
+
+//set view engine
+app.set("view engine", "ejs");
+//app.set("views", path.resolve(__dirname, "views/ejs"))
+
+// // load assets
+// app.use("/css", express.static(path.resolve(__dirname, "assets/css")));
+// app.use("/img", express.static(path.resolve(__dirname, "assets/img")));
+// app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is working on http://localhost:${process.env.PORT}`);
+});
